114250006ba55422ac0e9ef975f8300245d6644a
bikiin <Breyston@riservi.com>
2022-10-28 09:32:27 -0500

    se añadio al build el gitignore

--- build/services/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = require("path");
-const getView = (path) => {
-    return (0, path_1.normalize)((0, path_1.join)(__dirname, path));
-};
-exports.default = getView;
